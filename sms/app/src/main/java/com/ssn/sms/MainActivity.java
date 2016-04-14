c544c6378e4e12e61468bbd882ade24d1b4f2cbc
vishal5011g <vishal.vasudevan.vishal@gmail.com>
2016-04-14 12:11:23 +0530

    final

---
+++ sms/app/src/main/java/com/ssn/sms/MainActivity.java
@@ -0,0 +1,105 @@
+package com.ssn.sms;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements OnItemClickListener {
+
+    private static MainActivity inst;
+    ArrayList<String> smsMessagesList = new ArrayList<String>();
+    ListView smsListView;
+    ArrayAdapter arrayAdapter;
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+        smsListView = (ListView) findViewById(R.id.SMSList);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, smsMessagesList);
+        smsListView.setAdapter(arrayAdapter);
+        smsListView.setOnItemClickListener(this);
+        refreshSmsInbox();
+    }
+
+    public void refreshSmsInbox() {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor smsInboxCursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        int indexBody = smsInboxCursor.getColumnIndex("body");
+        int indexAddress = smsInboxCursor.getColumnIndex("address");
+        if (indexBody < 0 || !smsInboxCursor.moveToFirst()) return;
+        arrayAdapter.clear();
+        do {
+            String str = "SMS From: " + smsInboxCursor.getString(indexAddress) +
+                    "\n" + smsInboxCursor.getString(indexBody) + "\n";
+            arrayAdapter.add(str);
+        } while (smsInboxCursor.moveToNext());
+    }
+
+    public void updateList(final String smsMessage) {
+        arrayAdapter.insert(smsMessage, 0);
+        arrayAdapter.notifyDataSetChanged();
+    }
+
+
+    public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+        try {
+            String[] smsMessages = smsMessagesList.get(pos).split("\n");
+            String address = smsMessages[0];
+            String smsMessage = "";
+            for (int i = 1; i < smsMessages.length; ++i) {
+                smsMessage += smsMessages[i];
+            }
+
+            String smsMessageStr = address + "\n";
+            smsMessageStr += smsMessage;
+            Toast.makeText(this, smsMessageStr, Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(this, MainActivity.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+            PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+            Notification n  = new Notification.Builder(this)
+                    .setContentTitle(address)
+                    .setContentText("")
+                    .setSmallIcon(R.drawable.noti)
+
+                    .setContentIntent(pIntent)
+                    .setAutoCancel(true).build();
+
+
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0, n);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
